a191c5abc29e77ac0478f61c60ebdc9866880f44
woojin0906 <jwjley@naver.com>
2022-07-21 22:03:12 +0900

    이미지 검색 후 jtree

--- src/file/ImageJtree.java
+++ src/file/ImageJtree.java
@@ -2,7 +2,9 @@ package file;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -18,8 +20,10 @@ import java.net.URLEncoder;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -34,12 +38,20 @@ public class ImageJtree extends JFrame implements ActionListener{
 	private JPanel panelNorth;
 	private JLabel lblQues;
 	private JTextField tfWord;
-	private JButton btnSearch;
+	private JButton btnOpen;
+	private JPanel panelSearch;
+	private JPanel panelSave;
+	private JPanel panelBtn;
+	private JButton btnSave;
+	private JLabel lblSearch;
+	private JLabel lblSave;
+	private JTextField tfSave;
+	private String wordSearch;
 
 	public ImageJtree(String title) {
 		setTitle(title);
 		setLocation(250, 100);
-		setSize(350, 180);
+		setSize(350, 220);
 		setResizable(false);
 		setLayout(new BorderLayout());
 		
@@ -53,11 +65,11 @@ public class ImageJtree extends JFrame implements ActionListener{
 	private void setNorth() {
 		panelNorth = new JPanel();
 		panelNorth.setLayout(null);
-		panelNorth.setPreferredSize(new Dimension(300, 60));
+		panelNorth.setPreferredSize(new Dimension(300, 50));
 		
-		lblQues = new JLabel("검색할 단어를 입력하세요.");
-		lblQues.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 20));
-		lblQues.setBounds(20, 20, 250, 30);
+		lblQues = new JLabel("저장할 키워드 및 위치");
+		lblQues.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 17));
+		lblQues.setBounds(15, 14, 250, 30);
 		panelNorth.add(lblQues);
 		
 		add(panelNorth, BorderLayout.NORTH);
@@ -65,23 +77,53 @@ public class ImageJtree extends JFrame implements ActionListener{
 
 
 	private void setCenter() {
-		panelCenter = new JPanel();
-		panelCenter.setLayout(null);
-		panelCenter.setPreferredSize(new Dimension(300, 120));
+		panelCenter = new JPanel(new GridLayout(3, 1));
+
+		panelSearch = new JPanel();
+		panelSearch.setLayout(null);
+		
+		lblSearch = new JLabel("키워드");
+		lblSearch.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 15));
+		lblSearch.setBounds(15, 5, 100, 30);
+		panelSearch.add(lblSearch);
 		
 		tfWord = new JTextField(10);
-		tfWord.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 20));
-		tfWord.setBounds(20, 15, 200, 30);
+		tfWord.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 15));
+		tfWord.setBounds(80, 7, 230, 25);
 		tfWord.addActionListener(this);
+		panelSearch.add(tfWord);
+
+		panelSave = new JPanel();
+		panelSave.setLayout(null);
 		
-		panelCenter.add(tfWord);
+		lblSave = new JLabel("저장 위치");
+		lblSave.setBounds(15, 5, 100, 30);
+		lblSave.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 15));
+		panelSave.add(lblSave);
 		
-		btnSearch = new JButton("검색");
-		btnSearch.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 15));
-		btnSearch.setBounds(240, 15, 70, 30);
-		btnSearch.addActionListener(this);
+		tfSave = new JTextField(10);
+		tfSave.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 15));
+		tfSave.setBounds(80, 7, 160, 25);
+		tfSave.setEnabled(false);
+		tfSave.addActionListener(this);
+		panelSave.add(tfSave);
 		
-		panelCenter.add(btnSearch);
+		btnOpen = new JButton("열기");
+		btnOpen.setFont(new Font("210 맨발의청춘 L", Font.PLAIN, 13));
+		btnOpen.setBounds(250, 7, 60, 25);
+		btnOpen.addActionListener(this);
+		panelSave.add(btnOpen);
+		
+		panelBtn = new JPanel(new FlowLayout());
+		
+		btnSave = new JButton("저장");
+		btnSave.setPreferredSize(new Dimension(270, 25));
+		btnSave.addActionListener(this);
+		panelBtn.add(btnSave);
+		
+		panelCenter.add(panelSearch);
+		panelCenter.add(panelSave);
+		panelCenter.add(panelBtn);
 		
 		add(panelCenter, BorderLayout.CENTER);
 	}
@@ -89,7 +131,6 @@ public class ImageJtree extends JFrame implements ActionListener{
 
 	public static void main(String[] args) {
 		ImageJtree imgJ = new ImageJtree("테스트");
-		
 		
 	}
 
@@ -114,10 +155,13 @@ public class ImageJtree extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		if(obj == btnSearch || obj == tfWord) {
-			if(tfWord.getText() != null) {
-				String tfResult = tfWord.getText();
-				System.out.println(tfResult);
+		if(obj == btnSave) {
+			if (tfWord.getText().equals(""))
+				JOptionPane.showMessageDialog(this, "키워드를 입력하셔야 합니다");
+			else if (tfSave.getText().equals(""))
+				JOptionPane.showMessageDialog(this, "저장위치를 입력하셔야 합니다");
+			else {
+				wordSearch = tfWord.getText();
 				
 				this.dispose();
 				
@@ -125,7 +169,7 @@ public class ImageJtree extends JFrame implements ActionListener{
 				String restApiKey = "9fe52013af1c347f70a2ce56386eabbf";  // 개인 rest-api 키 입력
 				
 				try {
-					String text = URLEncoder.encode(tfResult, "UTF-8"); // 검색어
+					String text = URLEncoder.encode(wordSearch, "UTF-8"); // 검색어
 					String postParams = "src_lang=kr&target_lang=en&query=" + text;  // 파라미터
 					String apiURL = "https://dapi.kakao.com/v2/search/image?" + postParams;
 					URL url = new URL(apiURL);
@@ -165,11 +209,8 @@ public class ImageJtree extends JFrame implements ActionListener{
 				    // 배열을 가져옵니다.
 				    JSONArray jArray = jObject.getJSONArray("documents");
 
-				    String savePath = "C:\\dream_coding\\img\\"; // 이미지 저장 파일
+				    String savePath = tfSave.getText() + "\\"; // 이미지 저장 파일
 				    String fileFormat = "jpg";
-				    
-//				    int w = 250;
-//				    int h = 100;
 			
 				    JTreeTest jt = new JTreeTest();
 
@@ -184,11 +225,7 @@ public class ImageJtree extends JFrame implements ActionListener{
 				        saveImage(imgURL, saveFile, fileFormat);
 
 				        jt.getDog().add(new DefaultMutableTreeNode(saveFileName));
-				        
-				       // ImageRead imgRead = new ImageRead(i, saveFileName, w, h);
-				        
-				       // w += 20;
-				       // h += 20;
+
 				        System.out.println("image_url(" + i + "): " + imgURL);
 				        
 				        System.out.println();
@@ -202,6 +239,19 @@ public class ImageJtree extends JFrame implements ActionListener{
 				
 			}
 			
+		} else if(obj == btnOpen) {
+//			JFileChooser fc = new JFileChooser();
+//			fc.showOpenDialog(this);
+//			File selfile = fc.getSelectedFile(); 
+//			
+//			tfSave.setText(fc.getSelectedFile().getAbsolutePath());
+//			
+			JFileChooser jfc = new JFileChooser();
+			jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			jfc.showDialog(this, null);
+			File dir = jfc.getSelectedFile();
+
+			tfSave.setText(dir == null ? "" : dir.getPath());
 		}
 	}
 
--- src/file/JTreeTest.java
+++ src/file/JTreeTest.java
@@ -1,34 +1,64 @@
 package file;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.File;
+import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import imgjtree.Util;
+
 public class JTreeTest extends JFrame {
 
 	private DefaultMutableTreeNode dog;
 	private DefaultMutableTreeNode cat;
-	private JTree jt;
-
+	private JScrollPane sp;
+	private JSplitPane jsp;
+	private JScrollPane scp;
+	private Container con = this.getContentPane();
+	private DefaultMutableTreeNode root = new DefaultMutableTreeNode("내 컴퓨터");
+	private JTree jt = new JTree(root);
+	
+	
 	public JTreeTest() {
 		
 		setLocation(250, 300);
-		setSize(200, 200);
+		setSize(500, 500);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 		
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("ROOT");
+		con.setLayout(new BorderLayout());
+		
 		dog = new DefaultMutableTreeNode("C:");
 		cat = new DefaultMutableTreeNode("D:");
 		root.add(dog);
 		root.add(cat);
 		
-		jt = new JTree(root);
-		add(jt);
+		ImageIcon imageicon = new ImageIcon("C:/images/gosling.jpg");
+		JLabel img = new JLabel(imageicon);
+		
+		sp = new JScrollPane(jt);
+		jsp = new JSplitPane();
+		jsp.setLeftComponent(sp);
+		con.add("Center", jsp);
+		
+		scp = new JScrollPane(img);
+		jsp.setRightComponent(scp);
 		
 		setVisible(true);
 		
@@ -37,8 +67,6 @@ public class JTreeTest extends JFrame {
 	public DefaultMutableTreeNode getDog() {
 		return dog;
 	}
-
-
 }
 
 
